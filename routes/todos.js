ac4ef979a55e999c4989c61aa8acf6b4f67affeb
cwissy <meahchris@gmail.com>
2019-03-01 17:12:34 +0000

    async await and mongoose refactor

--- routes/todos.js
+++ routes/todos.js
@@ -1,66 +1,64 @@
 const express = require("express");
-const connect = require("../connect");
+const mongoose = require("mongoose");
 const shortid = require("shortid");
 const router = express.Router();
 
-let db;
+mongoose.connect(
+  "mongodb+srv://appservice:apps3rv1c3@cluster0-k3ydo.mongodb.net/newtodos?retryWrites=true",
+  { useNewUrlParser: true }
+);
 
-connect("todoapp").then(database => {
-  db = database;
+const ToDo = mongoose.model("ToDo", {
+  title: String,
+  completed: Boolean,
+  id: String
 });
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
   // GET / -> all of the todos in the db
-  db.collection("todos")
-    .find({})
-    .toArray()
-    .then(todos => {
-      res.json({ payload: todos });
-    })
-    .catch(err => {
-      res.status(500).json({ error: err });
-    });
+  try {
+    const todos = await ToDo.find({});
+    res.json({ payload: todos });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   // POST / -> create a new todo in the db
-  const todo = {
+  const data = {
     title: req.body.title,
     completed: false,
     id: shortid.generate()
   };
-  db.collection("todos")
-    .insert(todo)
-    .then(() => {
-      res.status(201).json({ message: "All good, created it" });
-    })
-    .catch(err => {
-      res.status(500).json({ error: err });
-    });
+
+  try {
+    const todo = new ToDo(data);
+    await todo.save();
+    res.status(201).json({ message: "All good, created it" });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   // GET /:id -> individual todo with the id given
-  db.collection("todos")
-    .findOne({ id: req.params.id })
-    .then(todo => {
-      res.json({ payload: todo });
-    })
-    .catch(err => {
-      res.status(500).json({ error: err });
-    });
+  try {
+    const todo = await ToDo.findOne({ id: req.params.id });
+    res.json({ payload: todo });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
-router.patch("/:id", (req, res, next) => {
+router.patch("/:id", async (req, res, next) => {
   // GET /:id -> individual todo with the id given
-  db.collection("todos")
-    .update({ id: req.params.id }, { $set: { completed: true } })
-    .then(() => {
-      res.json({ message: "Completed" });
-    })
-    .catch(err => {
-      res.status(500).json({ error: err });
-    });
+  try {
+    await ToDo.update({ id: req.params.id }, { $set: { completed: true } });
+    res.json({ message: "Completed" });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
 module.exports = router;
